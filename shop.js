a4a78a0525fe017e5829a58f4c3565238584e853
Sanket Koirala <koiralasanket11@gmail.com>
2023-09-07 23:48:07 -0500

    Shop JS file load products

--- shop.js
+++ shop.js
@@ -1,22 +1,27 @@
-const products = require('./data/all_products.json');
-console.log(products);
 
-  const productContainer = document.querySelector('.product-container');
+const productContainer = document.querySelector('.product-container-grid');
 
+fetch('shop_products.json')
+.then(response => response.json())
+.then(products=> {
   products.forEach(product => {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+  const productCard = document.createElement('div');
+  productCard.classList.add('product-card');
 
-    productCard.innerHTML = `
-      <div class="product-image" style="background-image: url('${product.photo}')">
-      </div>
-      <div class="product-details">
-        <h2 class="product-title">${product.name}</h2>
-        <p class="product-description">${product.description}</p>
-        <p class="product-price">${product.price}</p>
-        <a href="#" class="product-button">Add to Cart</a>
-      </div>
-    `;
+  productCard.innerHTML = `
+    <div class="product-image" style="background-image: url('${product.photo}')">
+    </div>
+    <div class="product-details">
+      <h2 class="product-title">${product.name}</h2>
+      <p class="product-description">${product.description}</p>
+      <p class="product-price">$${product.price}</p>
+      <a href="#" class="product-button">Add to Cart</a>
+    </div>
+  `;
+  productContainer.appendChild(productCard);
+});
+})
+.catch(error => {
+console.error('Error loading products:', error);
+});
 
-    productContainer.appendChild(productCard);
-  });
